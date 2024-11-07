adc85e3b4bfd86daf987c482237e024f628e03ae
Lutin <tclutin01@gmail.com>
2024-11-08 01:20:39 +0500

    added sex

--- app/src/main/java/com/example/myapplication/ui/fragments/activities/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/ui/fragments/activities/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.myapplication.ui.fragments.activities
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
 import com.example.myapplication.R
 import com.example.myapplication.databinding.ActivityMainBinding
@@ -17,6 +19,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
